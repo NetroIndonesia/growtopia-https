bd99dd93365e86cbad9ff519e372f17c72920028
Netro <116276652+NetroIndonesia@users.noreply.github.com>
2025-04-11 19:13:06 +0700

    Update server_data.php

--- db/server_data.php
+++ db/server_data.php
@@ -1,7 +1,12 @@
 server|127.0.0.1
-port|17092
-loginurl|gtps.playserver.me
-#maint|Maintenance
+port|17091
 type|1
-meta|beta.penis.com
+loginurl|gtps.playserver.me
+#maint|Server currently change hosting, please join discord.gg/mcps to get the latest host.
+
+beta_server|127.0.0.1
+beta_port|17091
+
+beta_type|1
+meta|localhost
 RTENDMARKERBS1001
